c873a6be5e508bcf1ef64ee0ed723e4a4e2be01c
EC2 Default User <ec2-user@ip-172-31-83-140.ec2.internal>
2022-01-26 20:04:42 +0000

    lec2

---
+++ lec2.py
@@ -0,0 +1,23 @@
+"""
+lec 2
+"""
+
+#print("hello world") # this is a single line comment
+
+#print ( type (123) )
+
+#print( type (123.) )
+
+#print( type ("123") )
+
+#print(" Hello " + "World ")
+#print(2*4)
+
+##print(my_int+1)
+
+#my_int=121
+#print(my_int)
+
+my_str = 'hello world'
+
+print(my_str.upper())
